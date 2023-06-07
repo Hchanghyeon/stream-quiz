7ec2eb84473f99298c1476640a1e26b16efefae2
황창현 <changhyeon.h@kakao.com>
2023-06-07 17:14:10 +0900

    feat: stream Quiz1 3문제, Quiz2 1문제 풀이

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -3,27 +3,52 @@ package com.mangkyu.stream.Quiz1;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> collect = csvLines.stream().map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobby -> Arrays.stream(hobby.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
+        return collect;
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> result = csvLines.stream().filter(item -> item[0].substring(0,1).equals("정"))
+                .map(line -> line[1].replaceAll("\\s",""))
+                .flatMap(hobby -> Arrays.stream(hobby.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
+        return result;
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
+
+        int answer = (int)csvLines.stream().
+                map(line -> check(line[2],0)).reduce(0, Integer::sum);
+
+        return answer;
+    }
+
+    int check(String checkStr, int num){
+        int index = checkStr.indexOf("좋아", num);
+        if(index >= 0){
+            return 1 + check(checkStr, index+2);
+        }
         return 0;
     }
 
